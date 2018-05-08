6fdcf4b443904a92eaef1111111a75983a432e36
Wellison <wellison28@gmail.com>
2018-05-07 23:15:34 -0300

    create routes and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    get '/entrar', to: 'devise/sessions#new'
+    get '/cadastrar', to: 'devise/registrations#new'
+    root to: 'home#index'
+  end
+
+
 end
